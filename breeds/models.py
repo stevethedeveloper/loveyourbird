334f11681e1582f3ad09d13e93867256deae610f
Stephen Walton <info@stevethedeveloper.com>
2018-05-23 17:47:11 -0700

    Added breeds app and models

--- breeds/models.py
+++ breeds/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Breed(models.Model):
+    """Common breeds of pet birds"""
+    common_name = models.CharField(max_length=255, blank=False)
+    binomial_name = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+    description_source = models.CharField(max_length=255, blank=True)
+    food = models.TextField(blank=True)
+    size = models.CharField(max_length=50, blank=True)
+    sound = models.TextField(blank=True)
+    talking = models.TextField(blank=True)
+    social = models.TextField(blank=True)
+    country_of_origin = models.CharField(max_length=255, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Return the common name"""
+        return self.common_name
