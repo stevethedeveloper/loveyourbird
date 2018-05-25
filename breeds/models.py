819a63e0c3154a4d9db3899c536dbe1b9520fcc7
Stephen Walton <info@stevethedeveloper.com>
2018-05-25 16:29:54 -0700

    Changing fields in breeds to allow null temporarily while importing data

--- breeds/models.py
+++ breeds/models.py
@@ -3,19 +3,19 @@ from django.db import models
 class Breed(models.Model):
     """Common breeds of pet birds"""
     common_name = models.CharField(max_length=255, blank=False)
-    binomial_name = models.CharField(max_length=255, blank=True)
-    summary = models.TextField(blank=True)
-    pet_information = models.TextField(blank=True)
-    description = models.TextField(blank=True)
-    description_source = models.CharField(max_length=255, blank=True)
-    food = models.TextField(blank=True)
-    size = models.CharField(max_length=50, blank=True)
-    sound = models.TextField(blank=True)
-    talking = models.TextField(blank=True)
-    social = models.TextField(blank=True)
-    breeding = models.TextField(blank=True)
-    country_of_origin = models.CharField(max_length=255, blank=True)
-    date_added = models.DateTimeField(auto_now_add=True)
+    binomial_name = models.CharField(max_length=255, blank=True, null=True)
+    summary = models.TextField(blank=True, null=True)
+    pet_information = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    description_source = models.CharField(max_length=255, blank=True, null=True)
+    food = models.TextField(blank=True, null=True)
+    size = models.CharField(max_length=50, blank=True, null=True)
+    sound = models.TextField(blank=True, null=True)
+    talking = models.TextField(blank=True, null=True)
+    social = models.TextField(blank=True, null=True)
+    breeding = models.TextField(blank=True, null=True)
+    country_of_origin = models.CharField(max_length=255, blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
     
     def __str__(self):
         """Return the common name"""
