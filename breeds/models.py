caa63d64349cd98b098425f41ac2fdc7113301d1
Stephen Walton <info@stevethedeveloper.com>
2018-05-25 18:27:47 -0700

    Added ckeditor-django

--- breeds/models.py
+++ breeds/models.py
@@ -1,19 +1,21 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Breed(models.Model):
     """Common breeds of pet birds"""
     common_name = models.CharField(max_length=255, blank=False)
     binomial_name = models.CharField(max_length=255, blank=True, null=True)
-    summary = models.TextField(blank=True, null=True)
-    pet_information = models.TextField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    summary = RichTextUploadingField(blank=True, null=True)
+    pet_information = RichTextUploadingField(blank=True, null=True)
+    description = RichTextUploadingField(blank=True, null=True)
     description_source = models.CharField(max_length=255, blank=True, null=True)
-    food = models.TextField(blank=True, null=True)
+    food = RichTextUploadingField(blank=True, null=True)
     size = models.CharField(max_length=50, blank=True, null=True)
-    sound = models.TextField(blank=True, null=True)
-    talking = models.TextField(blank=True, null=True)
-    social = models.TextField(blank=True, null=True)
-    breeding = models.TextField(blank=True, null=True)
+    sound = RichTextUploadingField(blank=True, null=True)
+    talking = RichTextUploadingField(blank=True, null=True)
+    social = RichTextUploadingField(blank=True, null=True)
+    breeding = RichTextUploadingField(blank=True, null=True)
     country_of_origin = models.CharField(max_length=255, blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True, null=True)
     
--- loveyourbird_com/settings.py
+++ loveyourbird_com/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # ckeditor
+    'ckeditor',
+    'ckeditor_uploader',
+    
     # My apps
     'breeds',
 ]
@@ -121,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media/'
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
