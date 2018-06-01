f00faf49b1bf3028f3aa95ebce76efb2af9da786
Stephen Walton <info@stevethedeveloper.com>
2018-05-31 19:40:23 -0700

    Added route for editing a breed's images using custom urls and made number of images clickable in breed list.

--- breeds/admin.py
+++ breeds/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse, path
+from django.http import HttpResponse
 
 from breeds.models import Breed, BreedImage
 
@@ -10,8 +13,27 @@ class BreedAdmin(admin.ModelAdmin):
     #inlines = [BreedImageInline]
     list_display = ('common_name', 'number_of_images')
 
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                '<int:breed_id>/images/',
+                self.admin_site.admin_view(self.edit_images),
+                name='edit_breed_images',
+            ),
+        ]
+        return custom_urls + urls
+        
     def number_of_images(self, obj):
-        return obj.breedimage_set.count()
+        return format_html(
+            '<a href="{}">' + str(obj.breedimage_set.count()) + '</a>',
+            reverse('admin:edit_breed_images', args=[obj.pk]),
+        )
+        number_of_images.short_description = 'Actions'
+        number_of_images.allow_tags = True
+            
+    def edit_images(self, request, breed_id, *args, **kwargs):
+        return HttpResponse(breed_id)
 
 admin.site.register(Breed, BreedAdmin)
-#admin.site.register(BreedImage)
+admin.site.register(BreedImage)
